1a41933feb255dd5f301cf3f79b7ca7c0ba7595b
garima94921 <gaarimabothra@gmail.com>
2020-07-11 23:07:54 +0530

    Initial Commit

---
+++ instaFilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instaFilter
+//
+//  Created by Garima Bothra on 11/07/20.
+//  Copyright © 2020 Garima Bothra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
