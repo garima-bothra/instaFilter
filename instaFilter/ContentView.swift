d57020e6d964b85a525b1a3928a04b5d1362ee47
garima94921 <gaarimabothra@gmail.com>
2020-07-12 06:53:12 +0530

    Added: ImagepickerView

--- instaFilter/ContentView.swift
+++ instaFilter/ContentView.swift
@@ -9,9 +9,59 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
+                    if image != nil {
+                        image?
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                    }
+                }
+                .onTapGesture {
+                    self.showingImagePicker = true
+                }
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                HStack {
+                    Button("Change Filter") {
+
+                    }
+                    Spacer()
+                    Button("Save") {
+
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("InstaFilter")
+            .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                ImagePicker(image: self.$inputImage)
+            }
+        }
     }
+
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
